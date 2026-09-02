659d9b9d05e8e5dded7e8935e58fa2b653c63345
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-02 15:13:40 +0530

    Create solution.sh

---
+++ starter/solution.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assignment:
+
+# Add a Linux user and display the details of the user.
+
+USERNAME="studentuser"
+
+# TODO 1:
+
+# Create the user using the useradd command.
+
+# TODO 2:
+
+# Display the details of the user using the id command.
+
+# TODO 3:
+
+# Exit successfully.
